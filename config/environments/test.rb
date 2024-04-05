e84d17892660fb576f606556fd7b81478b19a0b1
Tamara Temple <tamara@mrets.org>
2024-04-04 21:18:34 -0500

    Get one method of the weather api lib tested
    - add and install rspec-rails
    - add pry-rails and friends
    - add vcr and webmock
    - configure spec_helper
    - create weather_api library and spec, only one method tested so far
    - configure environments/* to add lib to eager and auto loading

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,6 +17,9 @@ Rails.application.configure do
   # loading is working properly before deploying your code.
   config.eager_load = ENV["CI"].present?
 
+  # Add lib/ to autoload and eager loading
+  config.autoload_lib(ignore: %w[assets tasks generators])
+
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
